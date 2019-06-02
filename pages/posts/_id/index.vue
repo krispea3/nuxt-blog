afb3ff9a45284496f4a98c403b55d296656b97fc
krispea3 <christian.bracher@gmail.com>
2019-06-02 14:38:35 +0800

    integrated basic posts detail

---
+++ pages/posts/_id/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <b-row class="text-center" align-h="center">
+    <b-col cols="8">
+      <b-card title="Card title" sub-title="Card subtitle">
+        <b-card-text>
+          Post-id: {{ postId }}
+          Some quick example text to build on the <em>card title</em> and make up the bulk of the card's
+          content.
+        </b-card-text>
+
+        <b-card-text>A second paragraph of text in the card.</b-card-text>
+
+        <a href="#" class="card-link">Card link</a>
+        <b-link to="/posts" class="card-link">Close</b-link>
+      </b-card>
+      </b-col>
+  </b-row>
+</template>
+
+<script>
+export default {
+  computed: {
+    postId () {
+      return this.$route.params.id
+    }
+  }
+}
+</script>
--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -1,5 +1,16 @@
 <template>
   <b-col>
     <p>List of posts</p>
+    <b-button variant="primary" @click="loadPost">Post detail</b-button>
   </b-col>
 </template>
+
+<script>
+export default {
+  methods: {
+    loadPost () {
+      this.$router.push('posts/' + '2')
+    }
+  }
+}
+</script>
