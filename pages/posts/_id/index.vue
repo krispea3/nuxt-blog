93ee97c26d5dc098d14d4baf4e53f00626cccb0b
krispea3 <christian.bracher@gmail.com>
2019-06-04 11:28:48 +0800

    removed component post and embedded it in posts/_id

--- pages/posts/_id/index.vue
+++ pages/posts/_id/index.vue
@@ -1,20 +1,26 @@
 <template>
   <b-row class="text-center" align-h="center">
     <b-col cols="8">
-      <Post :post="post"/>
+      <b-card :title="post.title"
+          img-src="~/assets/images/ai1.jpg" 
+          :img-alt="post.imgAlt" 
+          img-top>
+        <b-card-text>
+          Post ID = {{ post.id }}
+          This is a wider card with supporting text below as a natural lead-in to additional content.
+          This content is a little bit longer.
+        </b-card-text>
+        <b-button @click="$router.push('/posts')" variant="primary">Return</b-button>
+        <div slot="footer"><small class="text-muted">Last updated 3 mins ago</small></div>
+      </b-card>
     </b-col>
   </b-row>
 </template>
 
 <script>
-import Post from '~/components/post'
-
 export default {
   created () {
     this.$store.dispatch('setPost', this.$route.params.id)
-  },
-  components: {
-    Post
   },
   computed: {
     post () {
