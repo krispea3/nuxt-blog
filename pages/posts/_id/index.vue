560abe6d7ed78bd4e390d07506a469e32429e19d
krispea3 <christian.bracher@gmail.com>
2019-06-05 11:49:59 +0800

    added PostDetail to component and changed post/id page

--- pages/posts/_id/index.vue
+++ pages/posts/_id/index.vue
@@ -1,26 +1,16 @@
 <template>
-  <b-row class="text-center" align-h="center">
-    <b-col cols="8">
-      <b-card :title="post.title"
-          img-src="~/assets/images/ai1.jpg" 
-          :img-alt="post.imgAlt" 
-          img-top>
-        <b-card-text>
-          Post ID = {{ post.id }}
-          This is a wider card with supporting text below as a natural lead-in to additional content.
-          This content is a little bit longer.
-        </b-card-text>
-        <b-button @click="$router.push('/posts')" variant="primary">Return</b-button>
-        <div slot="footer"><small class="text-muted">Last updated 3 mins ago</small></div>
-      </b-card>
-    </b-col>
-  </b-row>
+  <PostDetail :post="post" />
 </template>
 
 <script>
+import PostDetail from '~/components/posts/PostDetail'
+
 export default {
   created () {
     this.$store.dispatch('setPost', this.$route.params.id)
+  },
+  components: {
+    PostDetail
   },
   computed: {
     post () {
