2e58fc96eac925e37222f4652a17478bb3c1f1dd
krispea3 <christian.bracher@gmail.com>
2019-06-05 13:03:11 +0800

    base functionality of form with firebase attachment

--- components/posts/PostForm.vue
+++ components/posts/PostForm.vue
@@ -1,67 +1,49 @@
 <template>
   <div>
-    <b-form @submit="onSubmit" @reset="onReset" v-if="show">
+    <b-form>
       <b-form-group
         id="input-group-1"
-        label="Email address:"
+        label="Title"
         label-for="input-1"
-        description="We'll never share your email with anyone else."
       >
         <b-form-input
           id="input-1"
-          v-model="form.email"
-          type="email"
+          v-model="formData.title"
+          type="text"
           required
-          placeholder="Enter email"
+          placeholder="Enter title"
         ></b-form-input>
       </b-form-group>
-
-      <b-form-group id="input-group-2" label="Your Name:" label-for="input-2">
-        <b-form-input
-          id="input-2"
-          v-model="form.name"
-          required
-          placeholder="Enter name"
-        ></b-form-input>
-      </b-form-group>
-
-      <b-form-group id="input-group-3" label="Food:" label-for="input-3">
-        <b-form-select
-          id="input-3"
-          v-model="form.food"
-          :options="foods"
-          required
-        ></b-form-select>
-      </b-form-group>
-
-      <b-form-group id="input-group-4">
-        <b-form-checkbox-group v-model="form.checked" id="checkboxes-4">
-          <b-form-checkbox value="me">Check me out</b-form-checkbox>
-          <b-form-checkbox value="that">Check that out</b-form-checkbox>
-        </b-form-checkbox-group>
-      </b-form-group>
-
-      <b-button type="submit" variant="primary">Submit</b-button>
+      <p>{{ post }}</p>
+      <b-button @click="saveForm" variant="primary">Save</b-button>
       <b-button type="reset" variant="danger">Reset</b-button>
     </b-form>
     <b-card class="mt-3" header="Form Data Result">
-      <pre class="m-0">{{ form }}</pre>
+      <pre class="m-0">{{ formData }}</pre>
     </b-card>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+
   export default {
+    created () {
+      if (this.post) {
+        this.formData.title = this.post.title
+        this.formData.text = this.post.text
+        this.formData.imgUrl = this.post.imgUrl
+        this.formData.imgAlt = this.post.imgAlt
+      }
+    },
     data() {
       return {
-        form: {
-          email: '',
-          name: '',
-          food: null,
-          checked: []
+        formData: {
+          title: '',
+          text: '',
+          imgUrl: '',
+          imgAlt: ''
         },
-        foods: [{ text: 'Select One', value: null }, 'Carrots', 'Beans', 'Tomatoes', 'Corn'],
-        show: true
       }
     },
     props: {
@@ -69,22 +51,32 @@
       required: false
     },
     methods: {
-      onSubmit(evt) {
-        evt.preventDefault()
-        alert(JSON.stringify(this.form))
+      saveForm () {
+        axios.post('https://nuxt-blog-9be94.firebaseio.com/post.json', this.formData)
+          .then(res => {
+            console.log(res)
+            this.$router.go(-1)
+          })
+          .catch(err => {
+            console.log(err)
+          })
       },
+      // onSubmit(evt) {
+      //   evt.preventDefault()
+        // alert(JSON.stringify(this.form))
+      // },
       onReset(evt) {
         evt.preventDefault()
         // Reset our form values
-        this.form.email = ''
-        this.form.name = ''
-        this.form.food = null
-        this.form.checked = []
+        this.formData.text = ''
+        this.formData.content = ''
+        this.formData.imgUrl = ''
+        this.formData.imgAlt = ''
         // Trick to reset/clear native browser form validation state
-        this.show = false
-        this.$nextTick(() => {
-          this.show = true
-        })
+        // this.show = false
+        // this.$nextTick(() => {
+        //   this.show = true
+        // })
       }
     }
   }
