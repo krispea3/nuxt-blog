12c835e261e51ee4275086cde81623bac9accadc
krispea3 <christian.bracher@gmail.com>
2019-06-06 11:35:55 +0800

    Initial posts fetch in nuxtSeverInit. Working

--- store/index.js
+++ store/index.js
@@ -2,29 +2,52 @@ import axios from 'axios'
 
 export const state = () => ({
   posts: [
-    { id: 1, 
-      title: 'Latest JS frameworks',
-      content: 'Some text to the first post',
-      imgUrl: 'https://qph.fs.quoracdn.net/main-qimg-bfa9bbf14a4f404c4d6f5c26c4fe4a99-c',
-      imgAlt: 'frameworks'
-    },
-    { id: 2, 
-      title: 'TensorFlow explained',
-      content: 'Some post text for TensorFlow',
-      imgUrl: 'https://thumbs.dreamstime.com/x/abstract-high-tech-background-vector-19785678.jpg',
-      imgAlt: 'ai'
-    },
+    // { id: 1, 
+    //   title: 'Latest JS frameworks',
+    //   content: 'Some text to the first post',
+    //   imgUrl: 'https://qph.fs.quoracdn.net/main-qimg-bfa9bbf14a4f404c4d6f5c26c4fe4a99-c',
+    //   imgAlt: 'frameworks'
+    // },
+    // { id: 2, 
+    //   title: 'TensorFlow explained',
+    //   content: 'Some post text for TensorFlow',
+    //   imgUrl: 'https://thumbs.dreamstime.com/x/abstract-high-tech-background-vector-19785678.jpg',
+    //   imgAlt: 'ai'
+    // },
   ],
   post: {}
 })
 
 export const mutations = {
+  loadPosts (state, posts) {
+    state.posts = posts
+  },
   setPost (state, post) {
     state.post = post
   }
 }
 
 export const actions = {
+  nuxtServerInit (vuexContext, context) {
+    return (
+      axios.get('https://nuxt-blog-9be94.firebaseio.com/post.json')
+        .then(res => {
+          const posts = []
+          for (const key in res.data) {
+            let post = res.data[key]
+            post.id = key
+            posts.push(post)
+          }
+          vuexContext.commit('loadPosts', posts)
+        })
+        .catch(err => {
+          context.error(err)
+        })
+    )
+    // if (req.session.user) {
+    //   commit('user', req.session.user)
+    // }
+  },
   setPost ({ commit, state }, postId) {
     const foundIndex = state.posts.findIndex(post => post.id == postId);
     const post = state.posts[foundIndex]
