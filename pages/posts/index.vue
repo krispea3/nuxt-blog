50d5ea3ee9888bf9c733fb2b3f117f541067a346
krispea3 <christian.bracher@gmail.com>
2019-06-21 11:57:17 +0800

    Removed unneeded middlewares and middleware calls

--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -9,9 +9,6 @@
 // import PostList from '~/components/posts/PostList'
 
 export default {
-  // Adding the middleware. Takes a string with the file-name without extension and without path
-  middleware: 'log',
-
   // No need to declare the component as it is defined in plugins
   // components: {
   //   PostList
