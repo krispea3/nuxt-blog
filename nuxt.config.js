5f567234dcd194bc8dc3d33b604761592317a140
krispea3 <christian.bracher@gmail.com>
2019-06-23 10:16:29 +0800

    Rechanged API_KEY.....

--- nuxt.config.js
+++ nuxt.config.js
@@ -119,7 +119,7 @@ export default {
     // Here we set the baseURL environment variable. If the server it's running on has one in process.env
     // It will take that. If it's not set it will take the hardcode one
     // baseURL: process.env.BASE_URL || 'https://nuxt-blog-9be94.firebaseio.com',
-    FB_API_KEY: AIzaSyC7ItGWr8uZpRAHrGC8_qztVg8QxMulzZE
+    FB_API_KEY: 'AIzaSyC7ItGWr8uZpRAHrGC8_qztVg8QxMulzZE'
   },
   // in router we can override all attributes of the router contructor
   route: {
